e69be6a9c7549b6a29b0664cb5fe328567bc2713
최영찬 <gks54410@gmail.com>
2022-08-29 23:15:18 +0900

    Feat: schema.sql 적용

--- hello-webflux/src/main/kotlin/com/example/hellowebflux/HelloWebfluxApplication.kt
+++ hello-webflux/src/main/kotlin/com/example/hellowebflux/HelloWebfluxApplication.kt
@@ -1,10 +1,22 @@
 package com.example.hellowebflux
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @SpringBootApplication
-class HelloWebfluxApplication
+class HelloWebfluxApplication {
+	@Bean
+	fun init(connectionFactory: ConnectionFactory) =
+		ConnectionFactoryInitializer().apply {
+			setConnectionFactory(connectionFactory)
+			setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+		}
+}
 
 fun main(args: Array<String>) {
 	runApplication<HelloWebfluxApplication>(*args)
