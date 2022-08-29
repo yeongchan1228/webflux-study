c31dfd2d0fb422f6f019be5bddba2bb99f90e772
최영찬 <gks54410@gmail.com>
2022-08-29 23:14:34 +0900

    Build: R2DBC, H2 Dependency 추가

--- hello-webflux/build.gradle.kts
+++ hello-webflux/build.gradle.kts
@@ -16,9 +16,14 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("io.r2dbc:r2dbc-h2")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
