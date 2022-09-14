aa42cd3bbbd4f40d0aae3ad72868210d43afc5c1
최영찬 <gks54410@gmail.com>
2022-09-14 23:43:33 +0900

    Initial commit

---
+++ spring-reactive-study/src/main/java/reactivestudy/springreactivestudy/SpringReactiveStudyApplication.java
@@ -0,0 +1,13 @@
+package reactivestudy.springreactivestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveStudyApplication.class, args);
+	}
+
+}
---
+++ spring-reactive-study/src/test/java/reactivestudy/springreactivestudy/SpringReactiveStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package reactivestudy.springreactivestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
