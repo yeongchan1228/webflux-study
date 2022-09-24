9321c11bb3b802f2829fbcd0fac771fa0120c227
최영찬 <gks54410@gmail.com>
2022-09-24 22:35:50 +0900

    Feat: DeferredResult 예제

--- spring-reactive-study/src/main/java/reactivestudy/springreactivestudy/reactive/async/SpringAsyncController.java
+++ spring-reactive-study/src/main/java/reactivestudy/springreactivestudy/reactive/async/SpringAsyncController.java
@@ -5,11 +5,10 @@ import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Queue;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -20,6 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class SpringAsyncController {
 
     static AtomicInteger counter = new AtomicInteger(0);
+    Queue<DeferredResult<String>> results = new ConcurrentLinkedDeque<>();
 
     @GetMapping("/basic")
     public String async() throws InterruptedException {
@@ -37,11 +37,34 @@ public class SpringAsyncController {
         };
     }
 
+    @GetMapping("/dr")
+    public DeferredResult<String> dr() throws InterruptedException {
+        log.info("dr");
+        DeferredResult<String> dr = new DeferredResult<>();
+        results.add(dr);
+        return dr;
+    }
+
+    @GetMapping("/dr/count")
+    public String drCount() {
+        return String.valueOf(results.size());
+    }
+
+    @GetMapping("/dr/event")
+    public String drEvent(String msg) {
+        for (DeferredResult<String> dr : results) {
+            dr.setResult("DrResult " + msg);
+            results.remove(dr);
+        }
+
+        return "OK";
+    }
+
     public static void main(String[] args) throws InterruptedException {
         ExecutorService es = Executors.newFixedThreadPool(100);
 
         RestTemplate restTemplate= new RestTemplate();
-        String url = "http://localhost:8080/callable";
+        String url = "http://localhost:8080/dr";
 
         StopWatch main = new StopWatch();
         main.start();
