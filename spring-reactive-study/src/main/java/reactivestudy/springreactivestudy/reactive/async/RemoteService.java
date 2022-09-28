6b60553931d11e8a9f2555ab5d1a20ab836df2b4
최영찬 <gks54410@gmail.com>
2022-09-28 20:10:05 +0900

    Refactor: ./v2 -> ./ 이동

--- spring-reactive-study/src/main/java/reactivestudy/springreactivestudy/reactive/async/RemoteService.java
+++ spring-reactive-study/src/main/java/reactivestudy/springreactivestudy/reactive/async/RemoteService.java
@@ -1,4 +1,4 @@
-package reactivestudy.springreactivestudy.reactive.async.v2;
+package reactivestudy.springreactivestudy.reactive.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
