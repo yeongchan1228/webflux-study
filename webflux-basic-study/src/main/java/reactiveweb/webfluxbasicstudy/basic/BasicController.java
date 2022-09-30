d6fadcc11a5ba078ac30b0f205b788296e8ff470
최영찬 <gks54410@gmail.com>
2022-09-30 19:35:14 +0900

    Feat: 비동기 API 호출 및 처리 코드 작성

--- webflux-basic-study/src/main/java/reactiveweb/webfluxbasicstudy/basic/BasicController.java
+++ webflux-basic-study/src/main/java/reactiveweb/webfluxbasicstudy/basic/BasicController.java
@@ -1,12 +1,17 @@
 package reactiveweb.webfluxbasicstudy.basic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by YC on 2022/09/28.
@@ -24,26 +29,32 @@ public class BasicController {
         this.myService = myService;
     }
 
-    /**
-     * 의존성이 존재하는 경우
-     */
     @GetMapping("/rest")
     public Mono<String> rest4(@RequestParam(required = false) int idx) {
-        Mono<String> result = webClient
-                .get()
-                .uri(Service1Uri, idx)
-                .retrieve()
-                .bodyToMono(String.class);
-
-        result.subscribe((res) -> System.out.println("res = " + res));
-
-        return Mono.just("Hello");
+        return webClient
+                .get().uri(Service1Uri, idx).retrieve().bodyToMono(String.class)
+                .flatMap(res -> webClient.get().uri(Service2Uri, res).retrieve().bodyToMono(String.class))
+                .doOnNext(res -> log.info(res))
+                .flatMap(res -> Mono.fromCompletionStage(myService.work(res)))
+                .doOnNext(res -> log.info(res));
     }
 
     @Service
     public static class MyService {
-        public String work(String req) {
-            return req + "/asyncWork";
+        @Async
+        public CompletableFuture<String> work(String req) {
+            return CompletableFuture
+                    .completedFuture(req + "/asyncWork");
         }
+    }
+
+    @Bean
+    ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setThreadNamePrefix("myThread");
+
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
     }
 }
