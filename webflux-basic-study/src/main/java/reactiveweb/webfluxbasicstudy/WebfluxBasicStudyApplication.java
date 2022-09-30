ccdf8109b5cf653c54ad6b2dc9abf4e17712ab9f
최영찬 <gks54410@gmail.com>
2022-09-30 16:36:29 +0900

    Initial commit

---
+++ webflux-basic-study/src/main/java/reactiveweb/webfluxbasicstudy/WebfluxBasicStudyApplication.java
@@ -0,0 +1,13 @@
+package reactiveweb.webfluxbasicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxBasicStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxBasicStudyApplication.class, args);
+	}
+
+}
