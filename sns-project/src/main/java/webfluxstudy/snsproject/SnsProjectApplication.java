91af95b10a913fe5e28af1bae29f233a612e2ff5
ycchoi <gks54410@gmail.com>
2022-11-13 17:42:01 +0900

    Initial Commit

---
+++ sns-project/src/main/java/webfluxstudy/snsproject/SnsProjectApplication.java
@@ -0,0 +1,13 @@
+package webfluxstudy.snsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsProjectApplication.class, args);
+	}
+
+}
